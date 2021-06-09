2daea90c449feb77764fd79013a1c369d8eabbea
Andrew Matte <andrew_matte_@hotmail.com>
2021-06-09 17:16:50 -0400

    before refactor

--- server/rec_algo/queries.py
+++ server/rec_algo/queries.py
@@ -1,6 +1,33 @@
 get_popular_taxonomies = "SELECT distinct taxonomy_code, taxonomy_name, count(*) FROM rec_data GROUP BY taxonomy_code, taxonomy_name ORDER BY 3 DESC;"
 
-get_cluster_constituents = "SELECT count(*), taxonomy_name FROM rec_data WHERE cluster_id = %s GROUP BY taxonomy_name ORDER BY 1 DESC LIMIT 10"
+get_cluster_data = "SELECT * FROM clusters WHERE cluster_id = %s"
+
+get_all_items_from_cluster = "SELECT resource_agency_number, public_name, resource_description FROM resources WHERE cluster_id = %s;"
+
+get_taxonomies_clusters = """SELECT 
+MODE() WITHIN GROUP (ORDER BY cluster_id) AS cluster_id,
+st.taxonomy_code
+FROM resources r
+INNER JOIN service_taxonomies st
+    ON r.resource_agency_number = st.resource_agency_number
+GROUP BY st.taxonomy_code;"""
+
+get_clusters_data = """SELECT *,
+(two_dim[0] - (select min(two_dim[0]) from clusters)) / (select max(two_dim[0]) - min(two_dim[0]) from clusters),
+(two_dim[1] - (select min(two_dim[1]) from clusters)) / (select max(two_dim[1]) - min(two_dim[1]) from clusters)
+FROM clusters c"""
+
+get_number_of_offered_services_in_cluster = """SELECT
+    count(*),
+    taxonomy_name
+FROM
+    resources r
+    INNER JOIN service_taxonomies st ON r.resource_agency_number = st.resource_agency_number
+WHERE
+    r.cluster_id = %s
+GROUP BY
+    taxonomy_name
+ORDER BY 1 DESC;"""
 
 get_all_vectors_and_IDs = """SELECT resource_agency_number, description_vector
     FROM resources
@@ -9,30 +36,12 @@ get_all_vectors_and_IDs = """SELECT resource_agency_number, description_vector
 get_distinct_taxonomies = """SELECT distinct taxonomy_code FROM rec_data;"""
 
 assign_cluster_id_to_taxonomy = """UPDATE
-        rec_data
+        rec_data as rd
     SET
-        cluster_ID = (
-            select cluster_id from
-            (SELECT
-                cluster_id,
-                count(*)
-            FROM
-                service_taxonomies st
-            INNER JOIN
-                resources r
-            ON
-                st.resource_agency_number = r.resource_agency_number
-            WHERE
-                taxonomy_code = %s
-            GROUP BY
-                cluster_id
-            ORDER BY
-                2 DESC
-            LIMIT
-                1) clusters
-        )
+        cluster_ID = e.cluster_id
+        FROM (VALUES %s) AS e(cluster_id, tax_code) 
     WHERE
-        taxonomy_code = %s;
+        taxonomy_code = e.tax_code;
     """
 
 create_recommendations_table = """CREATE TABLE IF NOT EXISTS rec_data (
@@ -59,7 +68,10 @@ insert_service_taxonomy = """INSERT INTO service_taxonomies
         );
         """
 
-assign_clusters_to_vectors = """UPDATE resources SET cluster_id = %s WHERE resource_agency_number = %s;"""
+assign_clusters_to_vectors = """UPDATE resources as r 
+SET cluster_id = e.cluster_id
+FROM (VALUES %s) as e(cluster_id, resource_agency_number)
+WHERE e.resource_agency_number = r.resource_agency_number;"""
 
 get_applicable_clusters_to_taxonomy = """SELECT cluster_id 
     FROM (
@@ -147,18 +159,17 @@ get_random_multiple_referral_call = """SELECT
                 multiples.call_report_number,
                 random()
             FROM
-                rec_data data
-                INNER JOIN (
+                 (
                     SELECT
                         call_report_number,
-                        count(distinct taxonomy_code)
+                        count(taxonomy_code) as counts
                     FROM
                         rec_data
                     GROUP BY
                         call_report_number
-                    HAVING
-                        count(distinct taxonomy_code) > 1
-                ) multiples ON data.call_report_number = multiples.call_report_number
+                ) multiples
+            WHERE
+                counts > 1
             ORDER BY 2 LIMIT 1
         ) one_id
         INNER JOIN rec_data rc ON rc.call_report_number = one_id.call_report_number;
--- server/rec_algo/recommendation_testing.py
+++ server/rec_algo/recommendation_testing.py
@@ -1,8 +1,9 @@
 import csv
 import os
 import psycopg2
-from sklearn import cluster
-from sklearn.cluster import Birch
+from psycopg2.extras import execute_values
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 import numpy as np
 from datetime import datetime
 import queries
@@ -13,16 +14,48 @@ def get_db():
     return connection.cursor(), connection
 
 
-def get_taxonomies():
+def setup_db():
+    cursor, connection = get_db()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS clusters (
+        cluster_id INT,
+        centre DOUBLE PRECISION[],
+        two_dim POINT,
+        num_services INT
+    );""")
+    connection.commit()
+    cursor.execute(
+        """ALTER TABLE resources ADD COLUMN cluster_id integer;""")
+    connection.commit()
+
+
+def get_all_taxonomies():
     cursor, connection = get_db()
     cursor.execute(queries.get_popular_taxonomies)
     return cursor.fetchall()
 
 
+def get_all_clusters():
+    cursor, connection = get_db()
+    cursor.execute(queries.get_clusters_data)
+    return cursor.fetchall()
+
+
+def get_cluster(cluster_id):
+    cursor, connection = get_db()
+    cursor.execute(queries.get_cluster_data, (cluster_id,))
+    cluster_row = cursor.fetchall()
+    cursor.execute(queries.get_all_items_from_cluster, (cluster_id,))
+    items = cursor.fetchall()
+    return {
+        "cluster": cluster_row,
+        "items": items,
+    }
+
+
 def get_recommended_clusters_from_taxonomies(taxonomies):
     cursor, connection = get_db()
     # sanitize inputs
-    ok_chars = ' *-.0123456789ABCDEFGHIJKLMNOPQRSTVWXYZ'
+    ok_chars = ' *-.,0123456789ABCDEFGHIJKLMNOPQRSTVWXYZ'
     for char in taxonomies:
         if char not in ok_chars:
             return {'error': 'bar character'}
@@ -31,8 +64,11 @@ def get_recommended_clusters_from_taxonomies(taxonomies):
     IDs = [x[0] for x in clusters]
     output = {}
     for ID in IDs:
-        cursor.execute(queries.get_cluster_constituents, (ID,))
-        output[str(ID)] = cursor.fetchall()
+        cursor.execute(queries.get_cluster_data, (ID,))
+        output[str(ID)] = {'cluster_data': cursor.fetchall()}
+        cursor.execute(
+            queries.get_number_of_offered_services_in_cluster, (ID,))
+        output[str(ID)]["taxonomies_included"] = cursor.fetchall()
     return output
 
 
@@ -52,11 +88,23 @@ def get_all_vectors_and_IDs():
 
 def assign_cluster_to_taxnomies():
     cursor, connection = get_db()
+    cursor.execute(queries.get_taxonomies_clusters)
+    pairs = cursor.fetchall()
+    data = []
+    for pair in pairs:
+        data.append([pair[0], pair[1]])
+    execute_values(cursor, queries.assign_cluster_id_to_taxonomy, data, page_size=300)
+    connection.commit()
+
+
+def fill_in_data():
+    cursor, connection = get_db()
     cursor.execute(queries.get_distinct_taxonomies)
     taxonomies = cursor.fetchall()
     for tax in taxonomies:
-        print(tax)
-        cursor.execute(queries.assign_cluster_id_to_taxonomy, (tax, tax,))
+        cursor.execute("""update service_taxonomies
+    set taxonomy_name = (select taxonomy_name from rec_data WHERE taxonomy_code = %s LIMIT 1)
+    WHERE taxonomy_code = (select taxonomy_code from rec_data WHERE taxonomy_code = %s LIMIT 1);""", (tax[0], tax[0],))
     connection.commit()
 
 
@@ -109,27 +157,69 @@ def load_taxonomy_codes_per_service_to_database():
     connection.commit()
 
 
+def dbify_vector(vector):
+    smaller_vector = []
+    for element in vector[0]:
+        smaller_vector.append(float(element))
+
+    return smaller_vector
+
+
 def assign_clusters(n_clusters=100):
     cursor, connection = get_db()
     # pull all vectors and IDs
     # cluster so that everything has a non-zero label
     # assign the labels back into the database
-    # get vectors
+    print('get vectors')
     vectors, index_to_ID = get_all_vectors_and_IDs()
     # creating clusterer
-    clusterer = Birch(n_clusters=n_clusters)
+    clusterer = KMeans(n_clusters=n_clusters)
     # fitting and predicting
+    print('fitting and predicting')
     labels = clusterer.fit_predict(vectors)
-    
-    #found all labels
-    try:
-        cursor.execute("""ALTER TABLE resources ADD COLUMN cluster_id integer;""")
-        connection.commit()
-    except:
-        cursor, connection = get_db()
+    print('label', len(set(labels)))
+
+    # found all labels
+    clusters_data = {}
+    for label in list(set(labels)):
+        # print(label)
+        clusters_data[str(label)] = {'label': label}
+        clusters_data[str(label)]['services_count'] = list(labels).count(label)
+        clusters_in_label = []
+        for idx in range(len(vectors)):
+            if labels[idx] == label:
+                clusters_in_label.append(vectors[idx])
+        clusters_in_label = np.asarray(clusters_in_label)
+        clusters_data[str(label)]['centre'] = np.asarray(np.matrix(
+            clusters_in_label).mean(0)[0])
+    centres = []
+    centre_IDs = []
+    for data in clusters_data:
+        clusters_data[data]['label']
+        centre_IDs.append(clusters_data[data]['label'])
+        centres.append(clusters_data[data]['centre'])
+    centres = np.asarray([c[0] for c in centres])
+    pca = PCA(n_components=2)
+    two_dim_points = pca.fit_transform(centres)
+    two_dim_dict = {}
+    for idx in range(len(centre_IDs)):
+        two_dim_dict[centre_IDs[idx]] = two_dim_points[idx]
+    cursor.execute("DELETE FROM clusters;")
+    connection.commit()
+    for data in clusters_data:
+        cursor.execute("""INSERT INTO clusters (cluster_id, centre, two_dim, num_services) VALUES (
+            {0}, array{1}, point({2}, {3}), {4}
+        );""".format(
+            clusters_data[data]['label'],
+            dbify_vector(clusters_data[data]['centre']), 
+            two_dim_dict[clusters_data[data]['label']][0], 
+            two_dim_dict[clusters_data[data]['label']][1], 
+            clusters_data[data]['services_count']))
+    data = []
     for i in range(len(labels)):
-        print(i)
-        cursor.execute(queries.assign_clusters_to_vectors, (int(labels[i]), index_to_ID[i]))
+        # print(i)
+        data.append([int(labels[i]), index_to_ID[i]])
+    execute_values(cursor, queries.assign_clusters_to_vectors, data, page_size=300)
     connection.commit()
 
 
@@ -145,7 +235,8 @@ def get_unique_cluster_ID_recommendations_from_basket(item_IDs):
     cursor, connection = get_db()
     cursor.execute(queries.get_applicable_clusters_to_taxonomy, (item_IDs,))
     existing_clusters = [x[0] for x in cursor.fetchall()]
-    cursor.execute(queries.get_restricted_cluster_recommendations, (existing_clusters, existing_clusters))
+    cursor.execute(queries.get_restricted_cluster_recommendations,
+                   (existing_clusters, existing_clusters))
     return cursor.fetchall()
 
 
@@ -156,8 +247,7 @@ def get_random_referral_group():
     return cursor.fetchall()
 
 
-def check_if_random_reference_is_recommendable(output_logs = False):
-    cursor, connection = get_db()
+def check_if_random_reference_is_recommendable(output_logs=False):
     base_group = get_random_referral_group()
     if output_logs:
         print('testing referral id:', base_group[0][0])
@@ -176,7 +266,8 @@ def check_if_random_reference_is_recommendable(output_logs = False):
     tally_item.append(str(recommended_cluster_labels))
     if output_logs:
         print(recommended_cluster_labels)
-    result, order = is_item_in_clusters(to_assess_to_algo, recommended_cluster_labels)
+    result, order = is_item_in_clusters(
+        to_assess_to_algo, recommended_cluster_labels)
     tally_item.append(str(result))
     tally_item.append(str(order))
     if output_logs:
@@ -185,7 +276,7 @@ def check_if_random_reference_is_recommendable(output_logs = False):
     return tally_item
 
 
-def check_if_random_unique_referrals_are_recommended(output_logs = False):
+def check_if_random_unique_referrals_are_recommended(output_logs=False):
     cursor, connection = get_db()
     base_group = get_random_referral_group()
     # call reference ID
@@ -212,7 +303,8 @@ def check_if_random_unique_referrals_are_recommended(output_logs = False):
     tally_item.append(str(recommended_cluster_labels))
     if output_logs:
         print(recommended_cluster_labels)
-    result, order = is_item_in_clusters(to_assess_to_algo, recommended_cluster_labels)
+    result, order = is_item_in_clusters(
+        to_assess_to_algo, recommended_cluster_labels)
     tally_item.append(str(result))
     tally_item.append(str(order))
     if output_logs:
@@ -221,7 +313,7 @@ def check_if_random_unique_referrals_are_recommended(output_logs = False):
     return tally_item
 
 
-def is_item_in_clusters(item, cluster_labels, output_logs = False):
+def is_item_in_clusters(item, cluster_labels, output_logs=False):
     cursor, connection = get_db()
     if output_logs:
         print('is cluster for', item, 'in clusters', cluster_labels)
@@ -239,20 +331,22 @@ def is_item_in_clusters(item, cluster_labels, output_logs = False):
     return working, c
 
 
-def recluster():
-    assign_clusters(200)
+def recluster(n_clusters=250):
+    assign_clusters(n_clusters)
     assign_cluster_to_taxnomies()
 
 
-def iterate(iterations=200):
-    tally = [['referral_id', 'taxonomy_to_find', 'to_feed_to_algo', 'recommended_cluster_labels', 'correct_labels', 'cluster_order', 'recommendation_time']]
+def iterate(how_many_tests=200):
+    tally = [['referral_id', 'taxonomy_to_find', 'to_feed_to_algo',
+              'recommended_cluster_labels', 'correct_labels', 'cluster_order', 'recommendation_time']]
     correct = 0
-    for _ in range(iterations):
+    for _ in range(how_many_tests):
         tally.append(check_if_random_reference_is_recommendable())
         # tally.append(check_if_random_unique_referrals_are_recommended())
         if tally[-1][4] == 'True':
             correct += 1
-        print('correct:', correct, 'out of:', _ + 1, '. Scheduled to do', iterations)
+        print('correct:', correct, 'out of:', _ + 1,
+              '. Scheduled to do', how_many_tests)
 
     f = open('results_non_unique.csv', 'a')
     for line in tally:
@@ -262,4 +356,5 @@ def iterate(iterations=200):
 
 
 if __name__ == '__main__':
-    iterate()
+    recluster(100)
+    # iterate()
