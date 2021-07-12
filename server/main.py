aeda670c59c27667d1e24748f641299aba5889b6
andrew.matte <andrew_matte_@hotmail.com>
2021-07-12 16:03:08 -0400

    fixed header interaction

--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
 # import 3rd party modules
+import requests
 from helper_classes.other_classes.cluster import Cluster
 from helper_classes.other_classes.clusterList import ClusterList
 from helper_classes.other_classes.taxonomyList import TaxonomyList
 from fastapi import FastAPI
-from fastapi import Header
+from fastapi.requests import Request
 import os
 import sentry_sdk
 from typing import Optional
@@ -21,6 +22,7 @@ from helper_classes.request_classes.geoSimilarRequest import GeoSimilarRequest
 from helper_classes.request_classes.searchRequest import SearchRequest
 import startup
 from fastapi.responses import HTMLResponse
+from fastapi import Header
 from services import cluster_explorer
 from services import cluster_recommendations
 
@@ -47,65 +49,72 @@ def session():
 
 
 @app.post("/add_item", response_model=Received)
-def add_item(basket_item: BasketItem, session_token: str):
+def add_item(basket_item: BasketItem, request: Request):
     """Adds item to session's basket.
     Ajoute objet au panier du session."""
+    session_token = request.headers.get('session_token')
     controllers.add_item(basket_item, session_token)
     return Received()
 
 
 @app.post("/remove_item", response_model=Received)
-def remove_item(basket_item: BasketItem, session_token: Optional[str] = Header(None, convert_underscores=False)):
+def remove_item(basket_item: BasketItem, request: Request):
     """Removes item from session's basket.
     Supprime un objet du panier du session."""
+    session_token = request.headers.get('session_token')
     controllers.remove_item(basket_item, session_token)
     return Received()
 
 
 @app.get("/items", response_model=ItemList)
-def get_items(session_token: Optional[str] = Header(None, convert_underscores=False)):
+def get_items(request: Request):
     """See what items you have.
     Voir quels object vous avez."""
+    session_token = request.headers.get('session_token')
     results = controllers.get_items(session_token)
     return results
 
 
 @app.post("/search", response_model=ItemList)
-def search(search_request: SearchRequest, session_token: Optional[str] = Header(None, convert_underscores=False)):
+def search(search_request: SearchRequest, request: Request):
     """Text search through resources and opportunities.
     Chercher a travers les resources et opportunites par texte.
     """
+    session_token = request.headers.get('session_token')
     results = controllers.search(
         session_token, search_request, app_state, vector_model)
     return results
 
 
 @app.get("/similar/{item_id}", response_model=ItemList)
-def get_item_by_id(item_id: str, session_token: Optional[str] = Header(None, convert_underscores=False)):
+def get_item_by_id(item_id: str, request: Request):
     """(EN) Returns similar resources and opportunities based on description text.
     Also stores the session-item pair in order to make future recommendations.
     (FR) Retourne des resources et opportunites selon le texte similier a l'object.
     """
+    session_token = request.headers.get('session_token')
     results = controllers.get_similar(
         session_token, item_id, app_state, vector_model)
     return results
 
 
 @app.post("/geosearch", response_model=ItemList)
-def get_geo_search(geo_search_request: GeoSearchRequest, session_token: Optional[str] = Header(None, convert_underscores=False)):
+def get_geo_search(geo_search_request: GeoSearchRequest, request: Request):
     """(EN) Returns resources and opportunities based on search, geographically constrained.
     (FR) Retourne des resources and opportunies selon le texte, proche des coordonees partages.
     """
+    session_token = request.headers.get('session_token')
     results = controllers.geo_search(
         session_token, geo_search_request, app_state, vector_model)
     return results
 
 
 @app.post("/similar", response_model=ItemList)
-def get_geo_search(geo_similar_request: GeoSimilarRequest, session_token: Optional[str] = Header(None, convert_underscores=False)):
+def get_geo_search(geo_similar_request: GeoSimilarRequest, request: Request):
     """(EN) Returns resources and opportunities based on search, geographically constrained.
     (FR) Retourne des resources and opportunies selon le texte, proche des coordonees partages.
     """
+    session_token = request.headers.get('session_token')
     results = controllers.geo_similar_search(
         session_token, geo_similar_request, app_state)
     return results
