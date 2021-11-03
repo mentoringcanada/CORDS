2c47e8bc40a4645295aea2a8ff32c138f090d8ca
Billy Hawkes <70971818+billyhawkes@users.noreply.github.com>
2021-11-02 20:52:24 -0400

    Fixed & Styled suggestion search button

--- client/src/components/ServicesOutput/RecommendBox/RecommendBox.tsx
+++ client/src/components/ServicesOutput/RecommendBox/RecommendBox.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 import LanguageContext from "../../../helper/LanguageContext";
 import SearchContext from "../../../pages/Search/SearchContext";
 import { StyledContainer } from "../../../styles/StyledContainer";
@@ -9,22 +10,41 @@ interface Props {
 }
 
 const RecommendBox = ({ suggestedSearches }: Props) => {
-    const { search } = useContext(SearchContext);
+    const { search, setSearch } = useContext(SearchContext);
     const { language } = useContext(LanguageContext);
+    const history = useHistory();
+
+    const StyledSuggestion = styled.a`
+        color: #333;
+        cursor: pointer;
+        :hover {
+            text-decoration: underline;
+        }
+    `;
+
+    const handleSuggestionSearch = (suggestion: string) => {
+        history.push(
+            `/search/results?ln=${language}&query=${suggestion}&distance=${search.distance}&lat=${search.location.lat}&lng=${search.location.lng}&filter=${search.filter}&source=${search.dataSource}&page=1`
+        );
+        setSearch({
+            ...search,
+            query: suggestion,
+        });
+    };
+
     return (
         <>
             {suggestedSearches.length > 0 && (
                 <StyledContainer>
                     <h3 style={{ marginBottom: "0.5rem" }}>
-                        Suggested for you based on:
+                        Suggested searches for you:
                     </h3>
                     {suggestedSearches.map((suggestion) => (
-                        <Link
-                            to={`/search/results?ln=${language}&query=${suggestion}&distance=${search.distance}&lat=${search.location.lat}&lng=${search.location.lng}&filter=${search.filter}&source=${search.dataSource}&page=1`}
-                            style={{ textDecoration: "none", color: "#333" }}
+                        <StyledSuggestion
+                            onClick={() => handleSuggestionSearch(suggestion)}
                         >
-                            <p>{suggestion}</p>
-                        </Link>
+                            {suggestion}
+                        </StyledSuggestion>
                     ))}
                 </StyledContainer>
             )}
