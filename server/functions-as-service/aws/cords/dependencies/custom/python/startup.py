2cf8359215444408327156de60e8e5c5367fe281
Austin Joyal <anaustinbeing@gmail.com>
2022-01-18 21:27:41 -0500

    Removed codes not needed for cords-search.

--- server/functions-as-service/aws/cords/dependencies/custom/python/startup.py
+++ server/functions-as-service/aws/cords/dependencies/custom/python/startup.py
@@ -1,37 +1,9 @@
-from helper_classes.other_classes.appState import AppState
 import model
-import numpy as np
-from services import cache
-from services import nlp_model
-from services import converters
 from services import pinecone_ops
 from sentence_transformers import SentenceTransformer
 import os
 
 pinecone_index = os.environ["PINECONE_INDEX"]
-
-
-def load():
-    print('loading NLP model')
-    vector_model = nlp_model.load_model()
-    print('loaded')
-
-    print('loading app state')
-    vectors_and_IDs = model.get_all_vectors()
-    cache.load_index()
-    vectors = np.asarray([converters.textvec2vec(row['description_vector']) for row in vectors_and_IDs])
-    index = cache.add_vectors(vectors)
-    app_state = AppState(
-        index_to_ID=[row['resource_agency_number'] for row in vectors_and_IDs],
-        cache=index)
-    print('loaded app state')
-    return app_state, vector_model
-
-def load_model():
-    print('loading NLP model')
-    model = nlp_model.load_model()
-    print('loaded')
-    return model
 
 def load_huggingface_model():
     print('loading hugging face paraphrase-multilingual-MiniLM-L12-v2 model')
